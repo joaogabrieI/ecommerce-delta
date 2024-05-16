4c137569c2d6b3f49c6974a85faf890619f032e1
joaogabrieI[ <joaonalves1@gmail.com>
2024-05-16 16:35:53 -0300

    agrupamento de rotas

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile/{user}', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
-    Route::post('/address/{user}', [AddressController::class, 'store'])->name('address.store');
-    Route::delete('/address/{address}', [AddressController::class, 'destroy'])->name('address.destroy');
-    Route::get('/address/{address}/edit', [AddressController::class, 'edit'])->name('address.edit');
-    Route::put('/address/{address}', [AddressController::class, 'update'])->name('address.update');
 });
 
 require __DIR__.'/auth.php';
