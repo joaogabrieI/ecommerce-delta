cc8a9187e7d1a28b21b21925ec32cc98fd3f250a
joaogabrieI[ <joaonalves1@gmail.com>
2024-05-24 15:35:19 -0300

    produtos por categoria fix

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,16 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="../style/main.css">
+    <link rel="stylesheet" href="{{asset('../style/main.css')}}">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
 </head>
 
-<body class="planodefundo-img">
-
-
+<body>
     <header>
         <div class="sidebar">
             <div class="logo-content">
