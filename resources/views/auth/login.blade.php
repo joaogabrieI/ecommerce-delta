f03738f50a27d82f8a5ec3975a30c8195e03a0b9
joaogabriei <joaonalves1@gmail.com>
2024-05-25 12:37:52 -0300

    ajuste login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,20 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
+        <link rel="stylesheet" href="{{asset('style/main.css')}}">
+
+    </head>
+    <body class="body-login">
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -13,24 +29,19 @@
                 <form method="post" action="{{ route('register') }}" class="form-login">
                     @csrf
                     <h1 class="title">Registre - se</h1>
-                    <input class="inputs" type="text" placeholder="Nome" name="name" :value="old('name')" required
-                        autofocus autocomplete="name">
+                    <input class="inputs" type="text" placeholder="Nome" name="name" :value="old('name')" required autofocus autocomplete="name">
                     <x-input-error :messages="$errors->get('name')" class="mt-2" />
 
-                    <input class="inputs" type="email" placeholder="Email" name="email" :value="old('email')" required
-                        autocomplete="username">
+                    <input class="inputs" type="email" placeholder="Email" name="email" :value="old('email')" required autocomplete="username">
                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
 
-                    <input class="inputs" type="text" placeholder="CPF" name="cpf" :value="old('cpf')" required
-                        autocomplete="cpf">
+                    <input class="inputs" type="text" placeholder="CPF" name="cpf" :value="old('cpf')" required autocomplete="cpf">
                     <x-input-error :messages="$errors->get('cpf')" class="mt-2" />
 
-                    <input class="inputs" type="password" placeholder="Senha" name="password" required
-                        autocomplete="new-password">
+                    <input class="inputs" type="password" placeholder="Senha" name="password" required autocomplete="new-password">
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
 
-                    <input class="inputs" type="password" name="password_confirmation" required
-                        autocomplete="new-password" placeholder="Confirmar senha">
+                    <input class="inputs" type="password" name="password_confirmation" required autocomplete="new-password" placeholder="Confirmar senha">
                     <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
 
                     <button>Cadastrar</button>
@@ -46,17 +57,12 @@
                 <form method="post" action="{{ route('login') }}" class="form-login">
                     @csrf
                     <h1 class="title">Login</h1>
-                    <input class="inputs" type="email" name="email" :value="old('email')" required autofocus
-                        autocomplete="username" placeholder="Email" id="email">
+                    <input class="inputs" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="Email" id="email">
                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
 
-                    <input class="inputs" type="password" name="password" required autocomplete="current-password"
-                        placeholder="Password" id="senha">
+                    <input class="inputs" type="password" name="password" required autocomplete="current-password" placeholder="Password" id="senha">
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
                     <button>Login</button>
-                    @if (Route::has('password.request'))
-                    <a href="{{ route('password.request') }}">{{ __('Esqueceu sua senha?') }}</a>
-                    @endif
                 </form>
             </div>
 
@@ -82,4 +88,5 @@
         </div>
     </section>
     <script src="{{asset('/js/login.js')}}"></script>
-</x-guest-layout>
+    </body>
+</html>
